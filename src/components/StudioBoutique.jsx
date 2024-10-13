8fba16bd8df77248f20d7d834803b188687e234f
Alex02CB <juanalejandrocb02@gmail.com>
2024-10-13 22:35:05 +0200

    Cambio a video principal en version movil

--- src/components/StudioBoutique.jsx
+++ src/components/StudioBoutique.jsx
@@ -35,7 +35,7 @@ function StudioBoutique() {
                                 Tu navegador no soporta el video HTML5.
                             </video>
                             <video autoPlay loop muted playsInline className="video-mobile">
-                                <source src="/imgBoutique/VideoBoutiqueMobile.mp4" type="video/mp4" />
+                                <source src="/imgBoutique/VideoBoutique.mp4" type="video/mp4" />
                                 Tu navegador no soporta el video HTML5.
                             </video>
                         </div>
